8d958fa441fa48009baa5c36311de1efb2ffff24
42ne 42ne <4itak1987@gmail.com>
2019-02-08 18:04:52 +0400

    feature: Авторизация (Google, Facebook) feature: привязка к БД

---
+++ GameBrowser/Models/Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimbirSoftDD_c_Study.Models
+{
+    public class Profile
+    {
+        public int No { get; set; }
+        public int RecordID { get; set; }
+        public string NickName { get; set; }
+        public int Score { get; set; }
+    }
+}
---
+++ GameBrowser/Models/settings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GameBrowser.Models
+{
+    public class Settings
+    {
+    }
+}
